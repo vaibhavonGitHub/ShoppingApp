ff583399a0d41ae229fc336f0ab40d27a8b3ece0
vaibhavonGitHub <69508218+vaibhavonGitHub@users.noreply.github.com>
2022-07-05 15:06:55 +0530

    Updates

---
+++ precache-manifest.71782e930386ed08f26920b84d16f6ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b932be14d977f68f44d",
+    "url": "/ShoppingApp/static/css/main.8ae196ba.chunk.css"
+  },
+  {
+    "revision": "8b932be14d977f68f44d",
+    "url": "/ShoppingApp/static/js/main.8b932be1.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/ShoppingApp/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "06dce84e2ff3eeb24c7e",
+    "url": "/ShoppingApp/static/js/2.06dce84e.chunk.js"
+  },
+  {
+    "revision": "248a21a6ed5c33bbc2ef",
+    "url": "/ShoppingApp/static/js/runtime~main.248a21a6.js"
+  },
+  {
+    "revision": "4da35bbb28b8689e03167acf8e460cdc",
+    "url": "/ShoppingApp/static/media/left-arrow.4da35bbb.svg"
+  },
+  {
+    "revision": "64c224f4d2f224353b11f05ad1822c71",
+    "url": "/ShoppingApp/static/media/right-arrow.64c224f4.svg"
+  },
+  {
+    "revision": "25472e1a0f0788baf38f146c4b09a771",
+    "url": "/ShoppingApp/index.html"
+  }
+];
